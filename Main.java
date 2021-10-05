309bdce67bd3bb0eadf68396f0e2a510cea3537b
saidasaduakas <80287291+saidasaduakas@users.noreply.github.com>
2021-10-05 15:05:04 +0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Purchase purchase= new getMoney(new Clothes(new Shopping()));
+        System.out.println(purchase.getPurchase());
+    }
+}
